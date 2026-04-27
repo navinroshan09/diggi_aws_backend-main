65e7e3a87ee04a027db944b754fdb9b3445c97a9
Navin Roshan Selvam <navinroshan295@gmail.com>
2026-04-27 15:11:32 +0530

    local link

--- api/index.py
+++ api/index.py
@@ -52,7 +52,7 @@ def insert_user(data):
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://thediggi.com", "http://www.thediggi.com"],
+    allow_origins=["http://thediggi.com", "http://www.thediggi.com", "http://localhost:5173", "http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
